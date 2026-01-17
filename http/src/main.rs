5e7e48a1cc755b81ce9fea70ff6067ac5097a7da
shreekar <me@shreekar.me>
2026-01-17 21:18:18 +0530

    just a listener

--- http/src/main.rs
+++ http/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use std::io;
+use std::io::Read;
+use std::str::from_utf8;
+
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    // saw multiple incoming connections possibility, how would syn-ack work then for tcp??? i am confused at this rn.
+    
+    match listener.accept() {
+        Ok((mut _socket, addr) )=> {
+            println!("new client connected from {addr:?}");
+            let mut buff:[u8; 1024] = [0; 1024];
+            let n = _socket.read(&mut buff)?;
+            println!("Received Raw data: {:?}", &buff[..n]);
+            println!("Received String data: {:?}", from_utf8(&buff[..n]));
+        },
+        Err(e) => {
+            println!("Error couldn't accept connection. : {e:?}")
+        }
+    }
+    
+
+    Ok(())
 }
